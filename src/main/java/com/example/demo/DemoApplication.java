9bc82b000faf99191924d0e3a7877eaa81e36000
Dave Syer <dsyer@vmware.com>
2022-10-19 13:56:05 +0100

    Upgrade to OpenAPI 3.0

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,23 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.example.demo.api.Bootstrap;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	ServletRegistrationBean<Bootstrap> bootstrap() {
+		ServletRegistrationBean<Bootstrap> registration = new ServletRegistrationBean<Bootstrap>(new Bootstrap(), "/bootstrap");
+		registration.setLoadOnStartup(2);
+		return registration;
 	}
 
 }
