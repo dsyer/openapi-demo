d24f57ccbb106a88d36bb0580b1fae1a0db6551d
Dave Syer <dsyer@vmware.com>
2022-10-19 17:59:16 +0100

    Endpoint works with explicit produces

---
+++ src/main/java/com/example/demo/StandardGeneratedApi.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import com.example.demo.api.GeneratedApi;
+import com.example.demo.model.Model;
+
+@Controller
+public class StandardGeneratedApi implements GeneratedApi {
+	@Override
+	public ResponseEntity<Model> generatedGet() {
+		Model model = new Model();
+		model.setName("Hi");
+		return ResponseEntity.ok().body(model);
+	}
+}
