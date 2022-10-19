850928f4999a8368f463da01604da7a2752ec2c6
Dave Syer <dsyer@vmware.com>
2022-10-19 10:28:58 +0100

    Manual and generated paths work
    And /swagger lists the generated resources!

--- src/main/java/com/example/demo/MyJerseyConfig.java
+++ src/main/java/com/example/demo/MyJerseyConfig.java
@@ -3,11 +3,18 @@ package com.example.demo;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import io.swagger.jaxrs.listing.ApiListingResource;
+import io.swagger.jaxrs.listing.SwaggerSerializers;
+
 @Component
 public class MyJerseyConfig extends ResourceConfig {
 
 	public MyJerseyConfig() {
 		register(MyEndpoint.class);
+		packages("io.swagger.jaxrs.listing",
+				"com.example.demo.api");
+		register(ApiListingResource.class);
+		register(SwaggerSerializers.class);
 	}
 
 }
