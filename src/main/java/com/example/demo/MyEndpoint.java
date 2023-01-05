0fe826c8ccb44259ce51e792721974b4401de1e5
Dave Syer <dsyer@vmware.com>
2023-01-05 11:07:07 +0000

    Check code generation and fix README

--- src/main/java/com/example/demo/MyEndpoint.java
+++ src/main/java/com/example/demo/MyEndpoint.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-@Component
+@RestController
 @RequestMapping("/manual")
 @Tag(name = "manual", description = "the manual API")
 public class MyEndpoint {
